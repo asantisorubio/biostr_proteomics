d61391c1c8a47f5d2500663fc64ccfe9a240fcf4
asantisorubio <150199112+asantisorubio@users.noreply.github.com>
2023-11-20 12:03:55 +0100

    biostr_proteomics.sh

--- biostr_proteomics.sh
+++ biostr_proteomics.sh
@@ -1,8 +1,4 @@
-# install the far superior mamba binary
-#conda install -y mamba
-#mamba create -y -n prot
-#source activate prot
-#mamba install git wget
+
 
 # create a directory with the files/subdirectories included in this github repository
 git clone https://github.com/asantisorubio/biostr_proteomics
@@ -17,22 +13,22 @@ wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase
 gunzip resources/proteome/human.fasta.gz
 
 # create a target-decoy database
-#mamba install -y decoypyrat
 decoypyrat resources/proteome/human.fasta -o resources/proteome/human.decoy.fasta --decoy_prefix DECOY
 cat resources/proteome/human.fasta resources/proteome/human.decoy.fasta > resources/proteome/human.target-decoy.fasta
 
 # search our input data (human study) against the target-decoy database
-#mamba install -y crux-toolkit
 crux comet --output-dir output/comet --overwrite T --output_txtfile 0 data/PXD003557/*.mgf resources/proteome/human.target-decoy.fasta
 
 # convert the pep.xml files into tsv files
 chmod u+x psm-convert.sh
 ./psm-convert.sh
 
-# merge the three tsv files into 1 to do the manual estimation of the FDR
+# merge the three tsv files into one to do the manual estimation of the FDR
 cat output/comet/comet.guojiao2_2.target.tsv <(tail -n +2 output/comet/comet.guojiao2_3.target.tsv) <(tail -n +2 output/comet/comet.guojiao2_3.target.tsv) > output/comet/comet.guojiao2_234.target.tsv
 # The mannual FDR calculation is in a folder inside ./output/
 
 # calculate the FDR/q value using assign-confidence and percolator. 
 crux assign-confidence --output-dir output/assign-confidence --overwrite T --decoy-prefix DECOY_ output/comet/*.pep.xml
 crux percolator --output-dir output/percolator --overwrite T --decoy-prefix DECOY_ output/comet/*.pep.xml
+
+# the comparison of the results is in the output folder.
